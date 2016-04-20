e948d1dd0a5d329099346098f4efd75c6b69b680
aucfan-igarashi <igarashi@aucfan.com>
2016-04-20 15:20:53 +0900

    tempate作成

--- rdbms/mysql/import.sh
+++ rdbms/mysql/import.sh
@@ -2,9 +2,15 @@
 
 set -x
 
+: "data defain" & {
+kogaki_url='http://www.post.japanpost.jp/zipcode/dl/kogaki/zip'
+oogaki_url='http://www.post.japanpost.jp/zipcode/dl/oogaki/zip'
+}
+
+
 : "data set and trancerate" & {
 cd /var/tmp
-wget http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip .
+wget ${kogaki_url}/ken_all.zip .
 unzip -c ken_all.zip | nkf -w | sed  's/"//g' > ken_all.csv
 }
 
