a583be7754197d243029d60b69f950055bcd83a7
aucfan-igarashi <igarashi@aucfan.com>
2016-04-21 17:15:00 +0900

    update

--- rdbms/mysql/import.sh
+++ rdbms/mysql/import.sh
@@ -5,13 +5,17 @@ set -x
 : "data defain" & {
 kogaki_url='http://www.post.japanpost.jp/zipcode/dl/kogaki/zip'
 oogaki_url='http://www.post.japanpost.jp/zipcode/dl/oogaki/zip'
+DIR='/var/tmp'
 }
 
 
 : "data set and trancerate" & {
-cd /var/tmp
+cd ${DIR}
 wget ${kogaki_url}/ken_all.zip .
 unzip -c ken_all.zip | nkf -w | sed  's/"//g' > ken_all.csv
 }
 
+: "all" & {
+cat ${DIR}/ken_all.csv | cut -d\, -f1-9 > ${DIR}/ken_data.csv
 
+}
--- rdbms/mysql/mysql-templates.sql
+++ rdbms/mysql/mysql-templates.sql
@@ -1,2 +1,11 @@
-CREATE DATABASE testdb4 CHARACTER SET utf8;
-CREATE TABLE    testdb4.testtable(col1 int, col2 varchar(30));
+CREATE DATABASE postdata CHARACTER SET utf8;
+CREATE TABLE    postdata.all(col1 int, 
+                             col2 varchar(30),
+                             col3 varchar(30),
+                             col4 varchar(30),
+                             col5 varchar(30),
+                             col6 varchar(30),
+                             col7 varchar(30),
+                             col8 varchar(30),
+                             col9 varchar(30)
+                            );
